0d2b70ee22a011ec75d5b9bd3fe7c5a7cb561254
Mrks83 <markus.pahs@gmx.net>
2016-01-27 13:16:30 +0100

    Use bundling

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,34 @@
 ﻿var gulp = require('gulp'),
 	fileinclude = require('gulp-file-include'),
-	markdown = require('gulp-markdown');
+	markdown = require('gulp-markdown'),
+	concat = require('gulp-concat');
 
-gulp.task('copy', function() {
-	gulp.src('./assets/**/*.*')
+gulp.task('copy', function () {
+	gulp.src([
+			'./assets/bootstrap/fonts/*.*',
+			'./assets/fonts/*.*'
+		])
+		.pipe(gulp.dest('./_gh_pages/assets/fonts'));
+
+	gulp.src([
+			'./assets/highlightJs/highlight.pack.js',
+			'./assets/jquery/jquery-1.11.2.min.js',
+			'./assets/bootstrap/js/bootstrap.min.js',
+			'./assets/sd/raphael-min.js',
+			'./assets/sd/underscore-min.js',
+			'./assets/sd/sequence-diagram-min.js'
+		])
+		.pipe(concat('vendors.js'))
+		.pipe(gulp.dest('./_gh_pages/assets/js'));
+
+	gulp.src('./assets/helppage.css')
+		.pipe(gulp.dest('./_gh_pages/assets'));
+
+	gulp.src([
+			'./assets/bootstrap/css/bootstrap.min.css',
+			'./assets/highlightJs/github.css'
+		])
+		.pipe(concat('vendors.css'))
 		.pipe(gulp.dest('./_gh_pages/assets/'));
 });
 
